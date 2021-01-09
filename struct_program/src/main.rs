b63b1e06afbf33da1714bf3603bc047110ea60ee
Brandon <brandon.haw@gmail.com>
2021-01-09 17:59:49 -0500

    Added enums, finished structs

--- struct_program/src/main.rs
+++ struct_program/src/main.rs
@@ -8,6 +8,17 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -15,25 +26,45 @@ fn main() {
     let height1 = 50;
 
     let rect1 = (30, 50);
+
     let rect2 = Rectangle {
         width: 30,
         height: 50,
     };
 
+    let rect3 = Rectangle {
+        width: 20,
+        height: 40,
+    };
+
+    let rect4 = Rectangle {
+        width: 40,
+        height: 60,
+    };
+
     println!("The area of the rectangle is {} square pixels.",
         area1(width1, height1)
     );
+
     println!("The area of the rectangle is {} square pixels.",
         area2(rect1)
     );
+
     println!("The area of the rectangle is {} square pixels.",
         area3(&rect2)
     );
+
     println!("The area of the rectangle is {} square pixels.",
         rect2.area()
     );
 
     println!("rect2 is {:#?}", rect2);
+
+    println!("Can rect 2 hold rect3? {}", rect2.can_hold(&rect3));
+    println!("Can rect 2 hold rect4? {}", rect2.can_hold(&rect4));
+
+    let square1 = Rectangle::square(32);
+    println!("{:#?}", square1);
 
 }
 
