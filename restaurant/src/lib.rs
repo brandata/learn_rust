086a46c52eaa0490b1d27492be8ef37e5b1e8241
Brandon <brandon.haw@gmail.com>
2021-01-11 18:01:20 -0500

    Updated modules again :)

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -12,6 +12,20 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
 }
 fn serve_order() {}
 
@@ -37,4 +51,8 @@ pub fn eat_at_restaurant() {
     
     //Relative
     front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 }
