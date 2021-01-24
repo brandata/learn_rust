cc4e5bc66485325355cf719a7bcf80e4e9ce2ea0
Brandon <brandon.haw@gmail.com>
2021-01-24 09:28:56 -0500

    Finished boilerplate

--- doug_milford/3d_demo/index.js
+++ doug_milford/3d_demo/index.js
@@ -1,2 +1,46 @@
 const rust = import('./pkg/rust_3d_demo');
+const canvas = document.getElementById('rustCanvas');
+const gl = canvas.getContext('webgl', {antialias: true});
 
+rust.then(m => {
+    if (!gl) {
+        alert('Faied to intialize WebGL');
+        return;
+    }
+
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+    const FPS_THROTTLE = 1000.0 / 30.0; // ms / frames
+    var lastDrawTime = -1; // in ms
+    const dougsClient = new m.DougsClient();
+    const initialTime = Date.now();
+
+    function render() {
+        window.requestAnimationFrame(render);
+        const currTime = Date.now()
+
+        if (currTime >= lastDrawTime + FPS_THROTTLE) {
+            lastDrawTime = currTime;
+
+            if (window.innerHeight != canvas.height || window.innerWidth != canvas.width) {
+                canvas.height = window.innerHeight;
+                canvas.clientHeight = window.innerHeight;
+                canvas.style.height = window.innerHeight;
+
+                canvas.width = window.innerWidth;
+                canvas.clientWidth = window.innerWidth;
+                canvas.style.width = window.innerWidth;
+
+                gl.viewport(0, 0, window.innerWidth, window.innerHeight);
+            }
+            
+            let elapsedTime = currTime - initialTime;
+            dougsClient.update(elapsedTime, window.innerHeight, window.innerWidth);
+            dougsClient.render();
+        
+        }
+    }
+
+    render();
+});
--- doug_milford/3d_demo/src/lib.rs
+++ doug_milford/3d_demo/src/lib.rs
@@ -11,3 +11,27 @@ extern "C" {
 pub fn say_hello_from_rust() {
     log("Hello from rust!");
 }
+
+#[wasm_bindgen]
+pub struct DougsClient {
+
+}
+
+#[wasm_bindgen]
+impl DougsClient {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        log("New was hit!");
+        Self {
+
+        }
+    }
+
+    pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
+        Ok(())
+    }
+
+    pub fn render(&self) {
+        
+    }
+}
